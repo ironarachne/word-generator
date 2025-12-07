7797ecd36c324b8698f917e502515c99ec7d2adc
Ben Overmyer <ben@overmyer.net>
2025-12-07 11:10:22 -0330

    Add digits

--- dist/elements.js
+++ dist/elements.js
@@ -6,6 +6,7 @@ import WordElementSet from "./elementset.js";
  * @since 1.0.0
  */
 export const allElements = [
+    new WordElementSet("digits", "#", ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]),
     new WordElementSet("affricates", "a", ["ch", "j"]),
     new WordElementSet("voiced dental plosive", "b", ["d", "dh"]),
     new WordElementSet("consonants", "c", [
